69520ce3499efe9803d8aa33ef5822dae7684e5d
filippopendyk <filipos28511@gmail.com>
2023-02-25 23:40:16 -0500

    removes the topic slice

---
+++ src/containers/Posts.tsx
@@ -0,0 +1,10 @@
+import { useAppSelector } from "../hooks";
+
+const Posts: React.FC = () => {
+
+    return (
+        <h1>This is Posts container about Posts</h1>
+    )
+}
+
+export default Posts;
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,22 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { resetTopic, setTopicAs } from "../features/topic/topicSlice";
 import { useAppDispatch } from "../hooks";
 
 const Feed: React.FC = () => {
     let { category } = useParams();
-
-    const dispatch = useAppDispatch();
-
-    // Sets the current category of posts
-
-    useEffect(() => {
-        if(typeof category === 'undefined'){
-            dispatch(resetTopic);
-        } else {
-            dispatch(setTopicAs(category.toLowerCase()));
-        }
-    }, [category]);
 
     return <h2>This is feed component with {category ? category : 'Home'} category!</h2>
 }
