af7461c9b38632b0f0fb5b525d1189c36d8e877c
filippopendyk <filipos28511@gmail.com>
2023-02-25 23:52:28 -0500

    commit

--- src/containers/Posts.tsx
+++ src/containers/Posts.tsx
@@ -1,9 +1,13 @@
 import { useAppSelector } from "../hooks";
 
-const Posts: React.FC = () => {
+type Props = {
+    category: string | undefined; 
+}
+
+const Posts: React.FC<Props> = ({category}) => {
 
     return (
-        <h1>This is Posts container about Posts</h1>
+        <h1>This is Posts container about {category}</h1>
     )
 }
 
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAppDispatch } from "../hooks";
+import Posts from "../containers/Posts";
 
 const Feed: React.FC = () => {
     let { category } = useParams();
 
-    return <h2>This is feed component with {category ? category : 'Home'} category!</h2>
+    return <Posts category = {category} />
 }
 
 
