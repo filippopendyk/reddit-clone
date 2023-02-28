d5a2e3d753500c216792911cc819d134d81efb03
filippopendyk <filipos28511@gmail.com>
2023-02-28 18:15:20 -0500

    creates a posts slice

--- src/containers/Posts/Posts.tsx
+++ src/containers/Posts/Posts.tsx
@@ -1,8 +1,13 @@
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { useEffect } from "react";
+
 type Props = {
     topic: string | undefined;
 }
 
 const Posts: React.FC<Props> = ({topic}) => {
+
+
 
     return (
         <h2>This is Posts container about {topic ? topic : 'Home'}</h2>
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { fetchMostPopularCategories } from "../topCategories/topCategoriesSlice";
+
+type RedditApiResponse = {
+    data: {
+        children: {
+            data: {
+            }
+        }[];
+    }
+};
+
+
+export const fetchPosts = createAsyncThunk<FetchPostsPayload>(
+    'posts/fetchPosts', async(topic) => {
+        try{
+            //Fetch posts from specified category
+            const response = await fetch(`https://www.reddit.com/${topic}.json`);
+            const data: RedditApiResponse = await response.json();
+
+            //Parse the response data
+            const posts = data.data.children.map((child) => child.data);
+            console.log(posts);
+
+            return { posts };
+        } catch (err) {
+            throw new Error('failed to fetch posts');
+        }
+    }
+)
+
+type PostsState = {
+    isLoading: boolean;
+    error: string | null;
+    data: {}[];
+}
+
+const initialState: PostsState = {
+    isLoading: false,
+    error: null,
+    data: [],
+}
+
+type FetchPostsPayload = {
+    posts: {}[];
+}
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchPosts.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(fetchPosts.fulfilled, (state, action: PayloadAction<FetchPostsPayload>) => {
+            state.isLoading = false;
+            state.data = action.payload.posts;
+        });
+        builder.addCase(fetchPosts.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message || 'Something went wrong';
+        });
+    }
+}
+);
