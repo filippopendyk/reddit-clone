5fc9b127f0da8830300bd316293fb8d73b03d53c
filippopendyk <filipos28511@gmail.com>
2023-03-01 00:38:50 -0500

    removes the comment feature

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,100 +1,68 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type RedditApiResponse<T> = {
-  data: {
-    children: {
-      data: T;
-    }[];
-  }
-}
-
-type Post = {
-  id: string;
-  title: string;
-  author: string;
-  score: number;
-  num_comments: number;
-  created_utc: number;
-}
-
-type Comment = {
-  id: string;
-  body: string;
-  author: string;
-  score: number;
-  created_utc: number;
-}
+type RedditApiResponse = {
+    data: {
+        children: {
+            data: {
+            }
+        }[];
+    }
+};
 
 type FetchPostsPayload = {
-  posts: Post[];
-  topic: string;
+    posts: {}[];
+    topic: string;
 }
 
 export const fetchPosts = createAsyncThunk<FetchPostsPayload, string>(
-  "posts/fetchPosts",
-  async (topic: string, { rejectWithValue }) => {
-    try {
-      const response = await fetch(
-        `https://www.reddit.com/r/${topic}.json`
-      );
-      const data: RedditApiResponse<Post> = await response.json();
+    'posts/fetchPosts', async(topic: string, thunkApi) => {
+        try{
+            //Fetch posts from specified category
+            const response = await fetch(`https://www.reddit.com/r/${topic}.json`);
+            const data: RedditApiResponse = await response.json();
 
-      const posts = data.data.children.map((child) => child.data);
-      const postIds = posts.map((post) => post.id);
+            //Parse the response data
+            const posts = data.data.children.map((child) => child.data);
+            console.log(posts);
 
-      const commentPromises = postIds.map(async (postId) => {
-        const response = await fetch(
-          `https://www.reddit.com/comments/${postId}.json`
-        );
-        const data: RedditApiResponse<Comment> = await response.json();
-        return data.data.children.map((child) => child.data);
-      });
-
-      const comments = await Promise.all(commentPromises);
-
-      const postsWithComments = posts.map((post, index) => ({
-        ...post,
-        comments: comments[index],
-      }));
-
-      return { posts: postsWithComments, topic };
-    } catch (err) {
-      return rejectWithValue("Failed to fetch posts");
+            return { posts, topic };
+        } catch (err) {
+            throw new Error('failed to fetch posts');
+        }
     }
-  }
-);
+)
 
-interface PostsState {
-  isLoading: boolean;
-  error: string | null;
-  data: Post[];
+type PostsState = {
+    isLoading: boolean;
+    error: string | null;
+    data: {}[];
 }
 
 const initialState: PostsState = {
-  isLoading: false,
-  error: null,
-  data: [],
-};
+    isLoading: false,
+    error: null,
+    data: [],
+}
 
 const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchPosts.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(fetchPosts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.data = action.payload.posts;
-      })
-      .addCase(fetchPosts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message ?? "Something went wrong";
-      });
-  },
-});
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchPosts.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(fetchPosts.fulfilled, (state, action: PayloadAction<FetchPostsPayload>) => {
+            state.isLoading = false;
+            state.data = action.payload.posts;
+        });
+        builder.addCase(fetchPosts.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message || 'Something went wrong';
+        });
+    }
+}
+);
 
 export const { reducer: postsSliceReducer } = postsSlice;
