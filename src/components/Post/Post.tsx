60b393364eaab35fd863a76b3f87f864b38ea4b4
filippopendyk <filipos28511@gmail.com>
2023-03-06 22:09:20 -0500

    displays time passed since creating post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,6 +2,8 @@ import { RedditPost } from "../../features/posts/postsSlice";
 import './Post.css';
 import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 import numeral from "numeral";
+import getTimeDifference from "../../utils/getTimeDifference";
+import * as moment from "moment";
 
 type Props = {
     post: RedditPost;
@@ -28,7 +30,7 @@ const Post: React.FC<Props> = ({post}) => {
                 }
                 <article className="text-container">
                     <p className="subreddit-name">{post.subreddit_name_prefixed}</p>
-                    <p className="author-time">Posted by {post.author} at {} </p>
+                    <p className="author-time">Posted by {post.author} {(getTimeDifference(post.created))} </p>
                     <p className="title">{post.title}</p>
                 </article>
             </div>
---
+++ src/utils/getTimeDifference.ts
@@ -0,0 +1,30 @@
+import moment from 'moment';
+
+export default function getTimeDifference(postTime: number): string{
+   const postTimestamp = postTime;
+   const date = new Date();
+   const currentTimestamp = date.getTime();
+   const seconds = Math.floor(currentTimestamp / 1000);
+   const difference = seconds - postTimestamp;
+   let output = '';
+   if (difference < 60) {
+    // Less than a minute has passed:
+    output = `${difference} seconds ago`;
+    } else if (difference < 3600) {
+    // Less than an hour has passed:
+    output = `${Math.floor(difference / 60)} minutes ago`;
+    } else if (difference < 86400) {
+    // Less than a day has passed:
+    output = `${Math.floor(difference / 3600)} hours ago`;
+    } else if (difference < 2620800) {
+    // Less than a month has passed:
+    output = `${Math.floor(difference / 86400)} days ago`;
+    } else if (difference < 31449600) {
+    // Less than a year has passed:
+    output = `${Math.floor(difference / 2620800)} months ago`;
+    } else {
+    // More than a year has passed:
+    output = `${Math.floor(difference / 31449600)} years ago`;
+}   
+    return output;
+}
