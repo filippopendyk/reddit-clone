8f39f640e99368f7a8246ccb76ce0056e3fe28a0
filippopendyk <filipos28511@gmail.com>
2023-03-10 08:29:59 -0500

    creates a slice for fetching single post

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type RedditApiResponse = {
+export type RedditApiResponse = {
     data: {
         children: {
             data: {
